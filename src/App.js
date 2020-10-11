eb44eb30852254712be18f44adae1041d5b2fff6
MeinanWang <71469608+MeinanWang@users.noreply.github.com>
2020-10-11 11:27:15 -0700

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Article from "./Article/Article";
 import DynamicArticle from "./DynamicArticle/DynamicArticle";
@@ -9,7 +9,11 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-      // put data fetching code here!
+      const response = await fetch(
+        "http://demo1390455.mockable.io/articles"
+      );
+      const responseJson = await response.json();
+      setFetchedData(responseJson);
     };
 
     if (isEmpty(fetchedData)) {
